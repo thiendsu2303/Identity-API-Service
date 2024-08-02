fc3fe58599a5a3e6130fd3aa76ae6f4b2ee2c978
thiendsu2303 <vithien23@gmail.com>
2024-08-02 21:56:17 +0700

    adding first create user api

---
+++ src/main/java/com/example/identity_api_service/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.example.identity_api_service.controller;
+
+import com.example.identity_api_service.dto.request.UserCreationRequest;
+import com.example.identity_api_service.entity.User;
+import com.example.identity_api_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    User createUser(@RequestBody UserCreationRequest request){
+        return userService.createUser(request);
+    }
+}
