5b7b5186c93fbb9ddc66a4c9b1e8db1fc3b54b22
thiendsu2303 <vithien23@gmail.com>
2024-08-02 21:33:21 +0700

    first commit

---
+++ src/main/java/com/example/identity_api_service/IdentityApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.identity_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityApiServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/identity_api_service/IdentityApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.identity_api_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityApiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
